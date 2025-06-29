cf149bc054e6f2e7576e0b656b228758ddf3dfed
Santiago Restrepo <santiago.kuma@gmail.com>
2025-06-29 16:58:33 -0500

    Add UnoCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
+  plugins: [vue(), UnoCSS(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
 
