e465233a8be810f83cdb0c403c41e46d4c0934a2
Santiago Restrepo <santiago.kuma@gmail.com>
2025-06-30 13:37:26 -0500

    TS debug

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import UnoCSS from 'unocss/vite'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/calendario-cmk/',
   plugins: [vue(), UnoCSS(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
